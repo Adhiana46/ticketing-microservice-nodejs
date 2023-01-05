83b1c48c2953803a2973e444ac7c0008dae414df
Adhiana <adhianamastur48@gmail.com>
2023-01-05 09:38:00 +0700

    build(auth): setup mongo, and connect to it

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import {
   currentUserRouter,
@@ -24,6 +25,18 @@ app.all("*", async () => {
 });
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+const start = async () => {
+  try {
+    mongoose.set("strictQuery", false); // prepare for mongoose v7
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
