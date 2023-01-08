46f07ce5849e83b3b417f4a5de86dc682b77e83a
Adhiana <adhianamastur48@gmail.com>
2023-01-08 21:56:44 +0700

    refactor(auth/testing): make app testable

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,6 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
 
-import {
-  currentUserRouter,
-  signinRouter,
-  signoutRouter,
-  signupRouter,
-} from "./routes";
-import { errorHandler } from "./middlewares";
-import { NotFoundError } from "./errors";
-
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false, // disable encryption, because we store jwt
-    secure: true, // must https
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async () => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   // check env variables
