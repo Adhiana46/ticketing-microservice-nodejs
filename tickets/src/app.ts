35a94a34e8999dbcf9b1880334edd7a544f8b33b
Adhiana <adhianamastur48@gmail.com>
2023-01-15 22:27:58 +0700

    build(tickets): initial setup tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError } from "@adhiana-ticketing/common";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // disable encryption, because we store jwt
+    secure: process.env.NODE_ENV !== "test", // must https
+  })
+);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
