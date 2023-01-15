8ba2f43f5ce4fce7b52ab8f11b966b822d440850
Adhiana <adhianamastur48@gmail.com>
2023-01-15 22:18:32 +0700

    fix(auth): dockerfile use --omit=dev, use env for MONGO_URI

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,10 +7,13 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
 
   try {
     mongoose.set("strictQuery", false); // prepare for mongoose v7
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (err) {
     console.log(err);
