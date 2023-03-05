9fbd17ff6e378f0474f23ef8cfca74d50b87d77c
Adhiana <adhianamastur48@gmail.com>
2023-03-06 00:17:16 +0700

    refactor(nats-streaming): refactor publisher

--- test-nats/src/publisher.ts
+++ test-nats/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 const stan = nats.connect("ticketing", "pub", {
   url: "http://localhost:4222",
@@ -7,21 +8,29 @@ const stan = nats.connect("ticketing", "pub", {
 stan.on("connect", () => {
   console.log("Publisher connected to NATS");
 
-  const data = JSON.stringify({
+  const publisher = new TicketCreatedPublisher(stan);
+
+  publisher.publish({
     id: "123",
     title: "Konser LISA Blackpink",
     price: 25,
   });
 
-  stan.publish(
-    "ticket:created",
-    data,
-    (err: Error | undefined, guid: string) => {
-      if (err != undefined) {
-        console.error("Failed publish a message: ", err);
-      } else {
-        console.log("Message published: ", guid);
-      }
-    }
-  );
+  // const data = JSON.stringify({
+  //   id: "123",
+  //   title: "Konser LISA Blackpink",
+  //   price: 25,
+  // });
+
+  // stan.publish(
+  //   "ticket:created",
+  //   data,
+  //   (err: Error | undefined, guid: string) => {
+  //     if (err != undefined) {
+  //       console.error("Failed publish a message: ", err);
+  //     } else {
+  //       console.log("Message published: ", guid);
+  //     }
+  //   }
+  // );
 });
