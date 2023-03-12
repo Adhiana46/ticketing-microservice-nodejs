ac505f62c81f24bd0ac400fa5a632e7d41b47b4b
Adhiana <adhianamastur48@gmail.com>
2023-03-12 20:38:04 +0700

    feat(order): add implementation of create order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from "express";
-import { requireAuth, validateRequest } from "@adhiana-ticketing/common";
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+  OrderStatus,
+  BadRequestError,
+} from "@adhiana-ticketing/common";
 import { body } from "express-validator";
 import mongoose from "mongoose";
+import { Ticket, Order } from "../models";
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   "/api/orders",
@@ -17,7 +26,36 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({ msg: "POST /api/orders" });
+    const { ticketId } = req.body;
+
+    // find the ticket from database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // make sure ticket is not already reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
+
+    // calculate expiration date
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // build the order and save to database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket: ticket,
+    });
+    await order.save();
+
+    // publish an event saying that an order was created
+
+    res.status(201).send(order);
   }
 );
 
