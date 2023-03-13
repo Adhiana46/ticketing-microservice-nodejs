6989e0cbeacaa6d72b5ac60bc5a32c3c569195ed
Adhiana <adhianamastur48@gmail.com>
2023-03-13 16:07:34 +0700

    feat(order): create delete/cancel order endpoint

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,8 +1,27 @@
 import express, { Request, Response } from "express";
+import { Order } from "../models";
+import {
+  NotAuthorizeError,
+  NotFoundError,
+  OrderStatus,
+} from "@adhiana-ticketing/common";
 const router = express.Router();
 
 router.delete("/api/orders/:id", async (req: Request, res: Response) => {
-  res.send({ msg: "DELETE /api/orders/:id" });
+  let order = await Order.findById(req.params.id);
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId != req.currentUser!.id) {
+    throw new NotAuthorizeError();
+  }
+
+  order.status = OrderStatus.CancelledUser;
+  order.save();
+
+  res.send(order);
 });
 
 export default router;
