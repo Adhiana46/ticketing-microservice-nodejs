e443d37527c2d0fceafcf0f960af530ef5c6b58f
Adhiana <adhianamastur48@gmail.com>
2023-03-14 13:36:14 +0700

    feat(order): create TicketUpdatedListener

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,27 @@
+import {
+  Listener,
+  Subjects,
+  TicketUpdatedEvent,
+} from "@adhiana-ticketing/common";
+import { Message } from "node-nats-streaming";
+import { QUEUE_GROUP_NAME } from "./queue-group-name";
+import { Ticket } from "../../models";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName: string = QUEUE_GROUP_NAME;
+
+  async onMessage(parsedData: TicketUpdatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(parsedData.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    const { title, price } = parsedData;
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
