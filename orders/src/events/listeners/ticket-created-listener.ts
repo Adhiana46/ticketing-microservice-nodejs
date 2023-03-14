d3edc8aa67c6b0baac977f324287928cbb925cf4
Adhiana <adhianamastur48@gmail.com>
2023-03-14 11:32:24 +0700

    chore(order): use constant for queue group name to avoid typo

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -4,10 +4,11 @@ import {
   TicketCreatedEvent,
 } from "@adhiana-ticketing/common";
 import { Message } from "node-nats-streaming";
+import { QUEUE_GROUP_NAME } from "./queue-group-name";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName: string = "order-service";
+  queueGroupName: string = QUEUE_GROUP_NAME;
 
   onMessage(parsedData: TicketCreatedEvent["data"], msg: Message): void {
     throw new Error("Method not implemented.");
