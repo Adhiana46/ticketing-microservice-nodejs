e33961e20957b740a1043d7d251f010a86f29f00
Adhiana <adhianamastur48@gmail.com>
2023-06-04 20:28:19 +0700

    feat(tickets): only show unreserved ticket

--- tickets/src/routes/showall.ts
+++ tickets/src/routes/showall.ts
@@ -4,7 +4,9 @@ import { Ticket } from "../model";
 const router = express.Router();
 
 router.get("/api/tickets", async (req: Request, res: Response) => {
-  let tickets = await Ticket.find({});
+  let tickets = await Ticket.find({
+    orderId: undefined,
+  });
 
   res.send(tickets);
 });
